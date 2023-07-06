f42db6569ea1efbb7fa8e98cb659661d311d15ac
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-06 13:54:20 +0100

    Use typing module on 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,7 +6,9 @@
     is the square of the int/float v and should be annotated as a float.
 """
 
+from typing import Union, Tuple
 
-def to_kv(k: str, v: int | float) -> tuple[str, float]:
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a Tuple of v and k"""
     return (k, float(v ** 2))
