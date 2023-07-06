930642781d6a0293abc643828dc5b51fe5b5b23d
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-06 11:45:13 +0100

    Add Task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Defines a type-annotated function sum_mixed_list which takes a
+    list mxd_lst of integers and floats and returns their sum as a float.
+"""
+
+def sum_mixed_list(mxd_list: list[int | float]) -> float:
+    """Returns the sum of mxd_list as a float"""
+
+    return sum(mxd_list)
