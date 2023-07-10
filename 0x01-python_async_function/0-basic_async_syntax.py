28153ce22ebfa416e9cbaa0af4750413a3dfa0ea
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-10 14:56:03 +0100

    Add DocString to function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10):
+    """Returns a random number between 0 and max_delay"""
     delay = random.uniform(0, max_delay)
     asyncio.sleep(delay)
     return delay
