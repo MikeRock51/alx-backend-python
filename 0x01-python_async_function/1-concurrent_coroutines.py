0ab676effe75db93e89e828b55c6b50b496e871e
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-10 15:50:41 +0100

    Sort with heapq

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,13 +2,19 @@
 """Execute multiple coroutines at the same time with async"""
 
 import asyncio
+from typing import List
+import heapq
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Returns a sorted list of delays"""
 
-    return sorted(await asyncio.gather(*(wait_random(max_delay)
-                                         for i in range(n))))
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    heapq.heapify(delays)
+
+    sortedDelays = [heapq.heappop(delays) for _ in range(n)]
+
+    return sortedDelays
