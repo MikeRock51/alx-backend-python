e64bcfc87a01a2adf2061de60fdac4d7a7dde579
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-29 10:03:53 +0100

    Add Task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,12 +17,13 @@
     The body of the test method should not be longer than 2 lines.
 """
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from parameterized import parameterized
+from unittest.mock import Mock, patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests utils methods for right output"""
+    """Tests utils.access_nested_map method for right output"""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -42,3 +43,20 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test that key error is raised"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests utils.get_json method"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True},),
+        ("http://holberton.io", {"payload": False},),
+    ])
+    def test_get_json(self, url, payload):
+        """Mock Test of utils.get_json method"""
+        mockResponse = Mock()
+        mockResponse.json.return_value = payload
+
+        with patch('requests.get', return_value=mockResponse) as mockGet:
+            response = get_json(url)
+            self.assertEqual(response, payload)
+            mockGet.assert_called_once_with(url)
